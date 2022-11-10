bd9feb1b053b0ab39a1ac26ac8a36918cf15e441
auguspo <augustpolet@gmail.com>
2022-11-10 00:19:31 -0300

    TP 8

---
+++ js/ejecutar.js
@@ -0,0 +1,18 @@
+function inclusive() {
+  document.body.style.backgroundColor = "black"
+  document.body.style.color = "white"
+  document.getElementById("aside").style.backgroundColor = "black"
+  document.getElementById("header").style.backgroundColor = "grey"
+  document.getElementById("aside").style.color = "white"  
+  document.getElementById("header").style.color = "white"
+  document.getElementById("cd1").style.backgroundColor = "grey"
+  document.getElementById("cd2").style.backgroundColor = "grey"
+  document.getElementById("cd3").style.backgroundColor = "grey"
+  document.getElementById("cd4").style.backgroundColor = "grey"
+ 
+ 
+}
+
+function copy() {
+  alert("Autor: Augusto Poletti. EMAIL: augustpolet@gmail.com")
+}
